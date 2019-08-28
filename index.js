fb7d6a88971ebcc82b22d88837883984f00288c9
richardcrng@gmail.com <richardcrng@gmail.com>
2019-08-28 09:33:08 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+class Driver {
+  constructor(name, dateStr) {
+    this.name = name
+    this.startDate = new Date(dateStr)
+  }
 
+  yearsExperienceFromBeginningOf(yearStr) {
+    const endYear = new Date(yearStr)
+    return endYear.getFullYear() - this.startDate.getFullYear()
+  }
+}
+
+const EAST_WEST = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+class Route {
+  constructor(beginingLocation, endingLocation) {
+    this.beginingLocation = beginingLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled() {
+    const verticalBlocks = Math.abs(this.beginingLocation.vertical - this.endingLocation.vertical)
+    const horizontalBlocks = Math.abs(EAST_WEST.indexOf(this.beginingLocation.horizontal) - EAST_WEST.indexOf(this.endingLocation.horizontal))
+    return verticalBlocks + horizontalBlocks
+  }
+
+  estimatedTime(isPeak = false) {
+    return isPeak
+      ? this.blocksTravelled() / 2
+      : this.blocksTravelled() / 3
+  }
+}
